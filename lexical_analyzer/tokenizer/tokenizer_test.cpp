ee8e992467e281f70e4dcd6d4377f1961c0630a8
Michael <micoraafat96@gmail.com>
2018-03-23 23:02:24 +0200

    added test

--- lexical_analyzer/tokenizer/tokenizer_test.cpp
+++ lexical_analyzer/tokenizer/tokenizer_test.cpp
@@ -13,9 +13,11 @@
 
 
 void tokenizer_test_1();
+void tokenizer_test_2();
 
 void test_tokenizer() {
     tokenizer_test_1();
+    tokenizer_test_2();
 }
 
 
@@ -80,6 +82,88 @@ void tokenizer_test_1() {
     int i2 = 0;
     string expectedClasses2[] = {"n7-end", "n1-end"};
     string expectedLexemes2[] = {"1010101", "10101110"};
+    TOKEN_TYPE expectedTypes2[] = {REAL_TOKEN, REAL_TOKEN};
+
+    tokenizer->tokenize(s2);
+    while (tokenizer->hasNext()) {
+        Token t = tokenizer->nextToken();
+
+        if (t.lexme != expectedLexemes2[i2] || t.token_class != expectedClasses2[i2]
+            || t.type != expectedTypes2[i2]) {
+            cout << "Tokenizer: test 2 error: " << i2 << endl;
+            cout << "Lexeme:\n Expected: " << expectedLexemes2[i2] << ", Actual: " << t.lexme << endl;
+            cout << "Class:\n Expected: " << expectedClasses2[i2] << ", Actual: " << t.token_class << endl;
+            cout << "Type:\n Expected: " << expectedTypes2[i2] << ", Actual: " << t.type << endl;
+
+            error = true;
+        }
+
+        i2++;
+    }
+
+
+    if (!error) {
+        cout << "Tokenizer Success..." << endl;
+    }
+}
+
+
+void tokenizer_test_2() {
+    DfaNode * n1 = new DfaNode("n1",false);
+    DfaNode * n2 = new DfaNode("n2-end",true);
+    DfaNode * n3 = new DfaNode("n3-end",true);
+    DfaNode * n4 = new DfaNode("n4-end",true);
+    DfaNode * n5 = new DfaNode("n5-end",true);
+    DfaNode * n6 = new DfaNode("n6-end",true);
+
+    DfaEdge * n1n2 = new DfaEdge('0', '0', n1, n2);
+    DfaEdge * n1n5 = new DfaEdge('1', '1', n1, n5);
+    DfaEdge * n2n2 = new DfaEdge('0', '0', n2, n2);
+    DfaEdge * n2n4 = new DfaEdge('1', '1', n2, n4);
+    DfaEdge * n3n1 = new DfaEdge('1', '1', n3, n1);
+    DfaEdge * n5n3 = new DfaEdge('1', '1', n5, n3);
+    DfaEdge * n4n1 = new DfaEdge('0', '0', n4, n1);
+    DfaEdge * n5n1 = new DfaEdge('0', '0', n5, n1);
+    DfaEdge * n4n6 = new DfaEdge('1', '1', n4, n6);
+    DfaEdge * n6n2 = new DfaEdge('0', '1', n6, n2);
+
+    n1->addEdge(n1n2);
+    n1->addEdge(n1n5);
+    n2->addEdge(n2n2);
+    n2->addEdge(n2n4);
+    n3->addEdge(n3n1);
+    n4->addEdge(n4n1);
+    n4->addEdge(n4n6);
+    n6->addEdge(n6n2);
+    n5->addEdge(n5n3);
+    n5->addEdge(n5n1);
+
+    auto tokenizer = new Tokenizer(n1);
+
+
+    string s1 = "0";
+    string s2 = "1010101 111010";
+    string s3 = "1010111001";
+    tokenizer->tokenize(s1);
+    int i = 0;
+    string expectedClasses[] = {"n2-end"};
+    string expectedLexemes[] = {"0"};
+    TOKEN_TYPE expectedTypes[] = {REAL_TOKEN};
+    bool error = false;
+    while (tokenizer->hasNext()) {
+        Token t = tokenizer->nextToken();
+        if (t.lexme != expectedLexemes[i] || t.token_class != expectedClasses[i]
+                || t.type != expectedTypes[i]) {
+            cout << "Tokenizer: test 1 error: " << i;
+            error = true;
+        }
+
+        i++;
+    }
+
+    int i2 = 0;
+    string expectedClasses2[] = {"n3-end", "n4-end"};
+    string expectedLexemes2[] = {"1010101", "111010"};
     TOKEN_TYPE expectedTypes2[] = {REAL_TOKEN, REAL_TOKEN};
 
     tokenizer->tokenize(s2);
