331b52b0fea10297725bf3cb8a7a34b9d3a41095
Bishoy N. Gendy <programajor@gmail.com>
2018-03-23 02:36:03 +0200

    start table minimization

--- transition_table/builder/TableBuilder.cpp
+++ transition_table/builder/TableBuilder.cpp
@@ -2,6 +2,7 @@
 // Created by programajor on 3/23/18.
 //
 
+#include <algorithm>
 #include "TableBuilder.h"
 
 TransotionTable TableBuilder::buildTransitionTable(vector<DfaNode *> nodes, vector<char> alphabets) {
@@ -18,11 +19,55 @@ TransotionTable TableBuilder::buildTransitionTable(vector<DfaNode *> nodes, vect
         transitions[transitions.size() - 1].push_back(phi); // under any input phi goes to itself
     }
 
-    for (int i = 0; i < alphabets.size(); i) {
-        char input = alphabets[i];
-        for (int j = 0; j < transitions.size(); j++) {
-//            if (nodes[j].)
+    for (int i = 0; i < transitions.size(); i++) {
+        for (int j = 0; j < alphabets.size(); j++) {
+            char input = alphabets[j];
+            if (nodes[i]->valid_transition(input)) {
+                transitions[i].push_back(nodes[i]->do_transition(input));
+            } else {
+                transitions[i].push_back(phi);
+            }
         }
     }
+    vector<string> alphaStrings;
+    for (int k = 0; k < alphabets.size(); ++k) {
+        alphaStrings.push_back(alphabets[k] + "-" + alphabets[k]);
+    }
+    minimizeTable(transitions, alphaStrings);
     return TransotionTable();
+}
+
+void TableBuilder::minimizeTable(vector<vector<DfaNode *>> transtions, vector<string> alphabets) {
+    vector<int> removed;
+    vector<vector<int>> depend;
+    vector<string> newInputs;
+    for (int i = 0; i < alphabets.size(); ++i) {
+        vector<int> vec;
+        depend.push_back(vec);
+        if (find(removed.begin(), removed.end(), i) != removed.end()) break;
+        for (int j = i + 1; j < alphabets.size(); ++j) {
+            bool flag = true;
+            for (int k = 0; k < transtions.size() - 1; ++k) {
+                if (transtions[k][i] != transtions[k][j]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                depend[i].push_back(j);
+            }
+        }
+    }
+
+    for (int i = 0; i < depend.size(); ++i) {
+        string input = alphabets[i];
+        for (int j = 0; j < depend[i].size(); ++j) {
+//            input = mergeInputs(input, alphabets[depend[i][j]]);
+        }
+        newInputs.push_back(input);
+    }
+}
+
+string mergeInputs(string s1, string s2) {
+
 }
--- transition_table/builder/TableBuilder.h
+++ transition_table/builder/TableBuilder.h
@@ -5,13 +5,17 @@
 #ifndef COMPILERS_TABLEBUILDER_H
 #define COMPILERS_TABLEBUILDER_H
 
-
 #include "../model/TransitionTable.h"
 #include "../../models/DfaNode.h"
 
 class TableBuilder {
 public:
     TransotionTable buildTransitionTable(vector<DfaNode *> nodes, vector <char> alphabets);
+
+    void minimizeTable(vector<vector<DfaNode *>> nodes, vector <string> alphabets);
+
+private:
+    string mergeInputs(string s1, string s2);
 };
 
 
